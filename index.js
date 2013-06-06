7e2358fdf4bd34ea760e7d222b4e5957fbf28cbf
gregallensworth <greg.allensworth@gmail.com>
2013-06-06 10:48:01 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+var QUALITATIVE_COLORS = [
+    '#8DD3C7',
+    '#FFFFB3',
+    '#BEBADA',
+    '#FB8072',
+    '#80B1D3',
+    '#FDB462',
+    '#B3DE69',
+    '#FCCDE5',
+    '#D9D9D9',
+    '#BC80BD',
+    '#CCEBC5',
+    '#FFED6F',
+];
+
+var SINGLE_BAR_COLOR = '#006D2C';
+
+
+
+function init () {
+}
